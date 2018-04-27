97973b99b6b2d908a0c4b1d50943f8b7e3583e49
kevinubau <ubau-13@live.com>
2018-04-27 08:52:56 -0600

    creado el formulario para cargar actividades, con componentes material design para elegir tiempo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/App';
+import './css/estilo.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 ReactDOM.render(
   <App />,
